432caad7553f99f3d60c189ac2eddc1b5ef95cc6
Sander Harjak <sharjak@gmail.com>
2025-01-30 09:15:16 +0200

    fix liquibase:update task

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -1,25 +1,35 @@
 plugins {
-    id("org.liquibase.gradle")
+    id("org.liquibase.gradle") version "2.2.0"
+    distribution
 }
+apply(plugin = "org.liquibase.gradle")
 
 dependencies {
-    implementation("org.liquibase:liquibase-core:_")
-    runtimeOnly("com.h2database:h2:_")
-
-    liquibaseRuntime("org.liquibase:liquibase-core:_")
-    liquibaseRuntime("info.picocli:picocli:_")
-    liquibaseRuntime("com.h2database:h2:_")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.22.0")
+    liquibaseRuntime("com.h2database:h2")
+    liquibaseRuntime("info.picocli:picocli:4.6.3")
 }
 
 liquibase {
     activities.register("main") {
         arguments = mapOf(
-            "changelogFile" to "/changelog/changelog.yaml",
+            "changelogFile" to "/changelog.yaml",
             "classpath" to "$projectDir/changelog",
             "url" to (project.findProperty("liquibaseDbUrl") ?: "jdbc:h2:file:$projectDir/LhvTestDatabase"),
             "username" to (project.findProperty("liquibaseUser") ?: "lhv_test_user"),
             "password" to (project.findProperty("liquibasePassword") ?: "lhv_test_password"),
-            "logLevel" to "info"
+            "logLevel" to "info",
+            "liquibaseHubMode" to "off"
         )
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from(".")
+            include("changelog/**")
+            into("/")
+        }
     }
 }
